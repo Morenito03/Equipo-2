b0d8cf9d10fd419f0cca9b883b28ea7063b8b097
Jose Luis Matos Sosa <142519959+jose020712@users.noreply.github.com>
2024-02-27 23:03:59 -0500

    ave.py

--- IA Python/modulos/ave.py
+++ IA Python/modulos/ave.py
@@ -1,4 +1,4 @@
-from animal import Animal
+from modulos.animal import Animal
 
 class Ave(Animal):
    def __init__(self, cantidad, periodo_gestacion, nombre_cientifico, nombre_comun, region_procedencia, jaula, patio, color_plumas):
